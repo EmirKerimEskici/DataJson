71e29987819ab1a864e9a4d55def6bb2e72f6e46
Emir Kerim Eskici <86002529+EmirKerimEskici@users.noreply.github.com>
2021-08-13 14:18:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,17 +8,14 @@
             <p><b>id</b>:${responseJson[i].id}</p>
             <p><span>name</span>: ${responseJson[i].name} </p>
             <p><span>username</span> :${responseJson[i].username}</p>
-            <p><span>address:</span> ${responseJson[i].address}</p>
             <p><span>street</span>: ${responseJson[i].address.street}</p>
            <p><span>suite</span>: ${responseJson[i].address.suite}</p>
            <p><span>city</span>: ${responseJson[i].address.city}</p>
            <p><span>zipcode</span>: ${responseJson[i].address.zipcode}</p>
-           <p><span>geo</span>: ${responseJson[i].geo}</p>
-           <p><span>lat</span>: ${responseJson[i].lat}</p>
-           <p><span>lng</span>: ${responseJson[i].lng}</p>
+           <p><span>lat</span>: ${responseJson[i].address.geo.lat}</p>
+           <p><span>lng</span>: ${responseJson[i].address.geo.lng}</p>
            <p><span>phone</span>: ${responseJson[i].phone}</p>
            <p><span>website</span>: ${responseJson[i].website}</p>
-           <p><span>company</span>: ${responseJson[i].company}</p>
            <p><span>name</span>: ${responseJson[i].company.name}</p>
            <p><span>catchPhrase</span>: ${responseJson[i].company.catchPhrase}</p>
            <p><span>bs</span>: ${responseJson[i].company.bs}</p>
